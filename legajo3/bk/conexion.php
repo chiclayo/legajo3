439288cd564149471a3e993722e7b932a6ebcbcf
florcitachumbe <chumbef1205@gmail.com>
2019-03-06 10:48:43 -0500

    color

--- legajo3/bk/conexion.php
+++ legajo3/bk/conexion.php
@@ -1,21 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Sarid
- * Date: 28/02/2019
- * Time: 10:43
- */
-
-function conectar(){
-    $user="root";
-    $pass="";
-    $server="localhost";
-    $db="salud";
-    $con=mysqli_connect($server,$user,$pass)or die("error al conectar la base de datos".mysqli_error());
-    mysqli_select_db($db,$con);
+$conexion = mysqli_connect('localhost', 'root','');
+mysqli_select_db($conexion, 'salud');
+$resultado = mysqli_query($conexion,"select id, nombre, contraseña");
 
 
-    return $con;
 
-}
+
+
+
+
 ?>
